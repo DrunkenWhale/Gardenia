b814d9d250a9fb6c65317fdbc0f9d97a16d24774
Pigeon377 <892197240@qq.com>
2022-04-26 08:44:26 +0800

    restart

--- src/main/scala/skiplist/mutable/SkipListNode.scala
+++
@@ -1,7 +0,0 @@
-package skiplist.mutable
-
-case class SkipListNode[T](
-                            key: Int,
-                            data: T,
-                            forwards: Array[SkipListNode[T]] = Array()
-                          )
