1e62b6bbb3e7cffa0148def5541d30efa9120ce2
Pigeon377 <892197240@qq.com>
2022-04-29 18:55:59 +0800

    feat:change project directory

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import com.gardenia.connect.MySQLConnection
+import com.gardenia.connect.{DataBaseManager, MySQLConnection}
 import com.gardenia.expor.tpe.{CaseClassType, ExportTypes}
 
 import java.sql.{Connection, DriverManager}
@@ -6,6 +6,9 @@ import scala.collection.mutable.ListBuffer
 
 @main
 def main(): Unit = {
+  DataBaseManager.register(MySQLConnection(
+    mysqlUrl = "jdbc:mysql://localhost:3306/test", user = "root", password = "3777777"
+  ))
   com.gardenia.expor.conduct.Export("").exportCode[CaseClassType]()
 }
 
--- src/main/scala/com/gardenia/expor/conduct/Export.scala
+++ src/main/scala/com/gardenia/expor/conduct/Export.scala
@@ -7,19 +7,22 @@ import scala.reflect.ClassTag
 
 class Export(override private[expor] val exportPath: String) extends ExportConduct {
 
-  def exportCode[T <: ExportTypes]()(implicit clsTag: ClassTag[T]): Unit = {
+  def exportSourceCode[T <: ExportTypes]()(implicit clsTag: ClassTag[T]): Unit = {
     val operationCode = {
       clsTag.runtimeClass.getSimpleName match
-        case "JsonType" => 1
-        case "JavaClassType" => 2
-        case "CaseClassType" => 3
+        case "JsonType" =>
+          val tableMetaList = TableMeta.getTableMetaList
+        //TODO to json (using rosemary)
+        case "JavaClassType" =>
+          val tableMetaList = TableMeta.getTableMetaList
+        //TODO to java class
+        case "CaseClassType" =>
+          val tableMetaList = TableMeta.getTableMetaList
+        //TODO to scala case class
         case x: String => throw new Exception("Unknown Type: " + x)
     }
-    DataBaseManager.register(MySQLConnection(
-      mysqlUrl = "jdbc:mysql://localhost:3306/test", user = "root", password = "3777777"
-    ))
-    val tableMetaList = TableMeta.getTableMetaList
-    println(tableMetaList)
+
+
   }
 
 }
