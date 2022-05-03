070844a8b02b5c4be1bba921d18a4a5aef300da5
Pigeon377 <892197240@qq.com>
2022-05-03 19:12:54 +0800

    feat: java class gen

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,5 @@
 import com.gardenia.connect.{DataBaseManager, MySQLConnection}
-import com.gardenia.expor.tpe.{CaseClassType, ExportTypes}
+import com.gardenia.expor.tpe.{CaseClassType, ExportTypes, JavaClassType}
 
 import java.sql.{Connection, DriverManager}
 import scala.collection.mutable.ListBuffer
@@ -9,6 +9,6 @@ def main(): Unit = {
   DataBaseManager.register(MySQLConnection(
     mysqlUrl = "jdbc:mysql://localhost:3306/test", user = "root", password = "3777777"
   ))
-  com.gardenia.expor.conduct.Export("C:\\Users\\Nephren\\Data\\Project\\Gardenia").exportSourceCode[CaseClassType]()
+  com.gardenia.expor.conduct.Export("C:\\Users\\Nephren\\Data\\Project\\Gardenia\\test").exportSourceCode[JavaClassType]()
 }
 
--- src/main/scala/com/gardenia/expor/conduct/Export.scala
+++ src/main/scala/com/gardenia/expor/conduct/Export.scala
@@ -1,7 +1,7 @@
 package com.gardenia.expor.conduct
 
 import com.gardenia.connect.{DataBaseManager, MySQLConnection, TableMeta}
-import com.gardenia.expor.conduct.Export.generatorCaseClass
+import com.gardenia.expor.conduct.Export.{generatorCaseClass, generatorJavaClass, typeMappingJavaType}
 import com.gardenia.expor.tpe.{ExportTypes, JsonType}
 
 import java.io.File
@@ -17,33 +17,96 @@ class Export(override private[expor] val exportPath: String) extends ExportCondu
       case "JsonType" =>
         val tableMetaList = TableMeta.getTableMetaList
       //TODO to json (using rosemary)
+
       case "JavaClassType" =>
         val tableMetaList = TableMeta.getTableMetaList
+        tableMetaList.foreach(
+          meta => {
+            val path: Path = Path.of(exportPath + File.separator + meta.name + ".java")
+            Files.createFile(path)
+            Files.write(path, generatorJavaClass(meta).getBytes(), StandardOpenOption.WRITE)
+          }
+        )
       //TODO to java class
+
       case "CaseClassType" => {
         val tableMetaList = TableMeta.getTableMetaList
         tableMetaList.foreach(
           meta => {
             val path: Path = Path.of(exportPath + File.separator + meta.name + ".scala")
-            println(path.getFileName)
             Files.createFile(path)
             Files.write(path, generatorCaseClass(meta).getBytes(), StandardOpenOption.WRITE)
           }
         )
       }
-      //TODO to scala case class
+
       case x: String => throw new Exception("Unknown Type: " + x)
   }
 
 }
 
 object Export {
-  private def generatorCaseClass(tableMeta: TableMeta): String = {
-    s"case class ${tableMeta.name}" +
-      tableMeta.columnList.map((name, tpe) => s"$name: ${typeMapping(tpe)}").mkString("(", ",", ")")
+
+  private def generatorJavaClass(tableMeta: TableMeta): String = {
+
+    val parameterList = tableMeta.columnList.map((name, tpe) =>
+      s"private ${typeMappingJavaType(tpe)} $name;"
+    ).mkString("\n")
+
+    val constructorParameters = tableMeta.columnList.map((name, tpe) => s"${typeMappingJavaType(tpe)} $name").mkString(",")
+
+    val constructorBody = tableMeta.columnList.map((name, tpe) => s"this.$name=$name;").mkString("\n")
+
+    val constructor =
+      s"""
+         |public ${tableMeta.name}($constructorParameters){
+         |   $constructorBody
+         |}
+         |""".stripMargin
+
+    val beanMethodList = tableMeta.columnList.map((name, tpe) => {
+      val t = typeMappingJavaType(tpe)
+      s"""
+         |public $t get${name.charAt(0).toUpper.toString.appendedAll(name.substring(1))}(){
+         |  return this.$name
+         |}
+         |
+         |public void set${name.charAt(0).toUpper.toString.appendedAll(name.substring(1))}($t $name){
+         |    this.$name = $name
+         |}
+         |""".stripMargin
+    }).mkString("\n")
+
+
+    s"""
+       |public class ${tableMeta.name}{
+       |
+       |$parameterList
+       |
+       |$constructor
+       |
+       |$beanMethodList
+       |
+       |}
+       |""".stripMargin
   }
 
-  private def typeMapping(typeName: String): String = {
+  private def typeMappingJavaType(typeName: String): String = {
+    typeName match
+      case "INT" => "int"
+      case "TEXT" | "CHAR" | "VARCHAR" => "String"
+      case "BIT" => "boolean"
+      case x => throw new Exception(s"Unknown SQL Type $x")
+  }
+
+  private def generatorCaseClass(tableMeta: TableMeta): String = {
+    s"case class ${tableMeta.name}" +
+      tableMeta.columnList.map((name, tpe) => s"$name: ${typeMappingScalaType(tpe)}").mkString("(", ",", ")")
+  }
+
+  private def typeMappingScalaType(typeName: String): String = {
+    // TODO
+    //expansion type mapping
     typeName match
       case "INT" => "Int"
       case "TEXT" | "CHAR" | "VARCHAR" => "String"
